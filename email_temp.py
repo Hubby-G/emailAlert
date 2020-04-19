987781705d553b2ac4029db62a042a5e84d6f3c7
Hubby-G <Hubby-G@users.noreply.github.com>
2020-04-19 10:32:49 -0600

    Working version. tested 19Apr20
    No email attachment. just text in the body of email

--- email_temp.py
+++ email_temp.py
@@ -10,35 +10,39 @@ import os       # allows access to system variables
 # import imghdr   # allows email of pictures
 from datetime import datetime
 # from time import sleep
-from email.message import EmailMessage
+# from email.message import EmailMessage
 
 smtpUser = os.environ.get('smtpUser') # using envirnoment variable for critical data
 smtpPass = os.environ.get('smtpPass')
+smtpUser = str(smtpUser)
+smtpPass = str(smtpPass)
+
+contacts = ('earthling471@gmail.com', 'laza.eccleston@gmail.com')
 
 conn = sqlite3.connect('/home/pi/greenhouse2020.db')
 c = conn.cursor()
 
-min_T=0
-max_T=100
 
 def get_min():
   global min_T
   c.execute('SELECT dt, MIN(temperature) FROM greenhouse ORDER BY dt ASC LIMIT 72;') # last 12hours
-  min_T = c.fetchall()
+  min_T = c.fetchall() # this outputs a list class item
+  min_T = str(min_T[0]) # return a str to main program
+  return
   
-def emailAlert():
+def emailAlert(recip):
   global min_T
   global smtpUser
   global smtpPass
-  toAdd = 'earthling471@gmail.com'
+  
+  toAdd = recip
   fromAdd = smtpUser
   subject = "Minimum Temperature in the Greenhouse overnight"
   header = 'To: ' + toAdd + '\n' + 'From: ' + fromAdd + '\n' + 'Subject: ' + subject
-  body = 'The Minimum Temperature overnight was : ' + str(min_T) + '\n\n\n' + 'Message sent from Greenhouse pi'
+  body = 'The Minimum Temperature overnight was : ' + min_T + '\n\n\n' + 'Message sent from Greenhouse pi'
 
   s = smtplib.SMTP('smtp.gmail.com', 587)    # gmail also uses port 465, try it if this one gives you trouble
   #s = smtplib.SMTP('smtp.office365.com', 587)    #outlook uses this smtp server and port number
-
   # This sets us up for encryption, before sending username and password over the internet
   s.ehlo()
   s.starttls()
@@ -46,8 +50,12 @@ def emailAlert():
   s.login(smtpUser, smtpPass)  # logging into server to send the email
   s.sendmail(fromAdd, toAdd, header + '\n\n' + body)  # declares to who it is sent. needs 2 linefeeds
 
-  print('Temperature message sent' + '\n' + '--------------->')
+  s.quit           # when the email is sent, we close the server
+  return
 
-  s.quit           # when the email is sent, we close the server  
-  return  
+get_min()
+for recip in contacts:
+  emailAlert(recip)
+print ('COMPLETED!')
+
 
